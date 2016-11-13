b7e57280c5a1f41583cf4213080194f1a8267cff
yuntara <yuntara@mail.yuntara.net>
2016-11-13 21:12:17 +0900

    definitions

--- reverb/hrtf.h
+++ reverb/hrtf.h
@@ -1,7 +1,14 @@
 #pragma once
 #include <fstream>
 #include "combolution.h"
+/*
+F(w) = ÅÁf(t) e^(-iwt) dt
 
+ÅÁf(t-X) e^(-iwt) dt
+=ÅÁf(t-X)e^(-iw(t-X))* e^(-iwX) dt
+=e^(-iwX) * F(w)
+
+*/
 namespace hrtf {
 	class Hrtf :public combolution::Filter{
 	private:
